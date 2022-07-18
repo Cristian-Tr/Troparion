990f8825eaac9bc1cac809caf3ba75c6da73b32d
Cristian-Tr <106551922+Cristian-Tr@users.noreply.github.com>
2022-07-18 21:05:10 +0300

    Add files via upload

--- app1.js
+++ app1.js
@@ -11,7 +11,26 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
 
-//Calendar with the most important religious holidays
+  //Data
+  var objToday = new Date(),
+    weekday = new Array('Duminică', 'Luni', 'Marți', 'Miercuri', 'Joi', 'Vineri', 'Sâmbătă'),
+    dayOfWeek = weekday[objToday.getDay()],
+    domEnder = function () { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return ""; a = parseInt((a + "").charAt(1)); return 1 == a ? "" : 2 == a ? "" : 3 == a ? "" : "" }(),
+    dayOfMonth = today + (objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
+    months = new Array('Ianuarie', 'Februarie', 'Martie', 'Aprilie', 'Mai', 'Iunie', 'Iulie', 'August', 'Septembrie', 'Octombrie', 'Noiembrie', 'Decembrie'),
+    curMonth = months[objToday.getMonth()],
+    curYear = objToday.getFullYear(),
+    curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
+    curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
+    curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
+    curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
+  var today = dayOfWeek + " " + dayOfMonth + " " + curMonth + " " + curYear;
+
+  document.querySelector('#dateSaint').innerHTML = today;
+
+
+
+  //Calendar with the most important religious holidays
   const items = document.querySelectorAll('#timeline li');
 
   const isInViewport = el => {
